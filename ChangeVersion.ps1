ee4664b1d7b278519ac5f67ff81adcfe9d087fcb
Christian Rondeau <christian.rondeau@gmail.com>
2017-07-07 21:00:01 -0400

    Update NuGet executable version

---
+++ ChangeVersion.ps1
@@ -0,0 +1,25 @@
+﻿[CmdletBinding()]
+Param(
+	[String][Parameter(Mandatory=$True)]$Version
+)
+
+Set-StrictMode -version Latest
+$ErrorActionPreference = "Stop"
+
+Write-Host "Updating version number to $Version" -ForegroundColor Green
+
+Function AssignVersionToFile {
+	[CmdletBinding()]
+	Param (
+		[String][Parameter(Mandatory=$True)]$Path,
+		[String][Parameter(Mandatory=$True)]$RegEx,
+		[String][Parameter(Mandatory=$True)]$Replacement
+	)
+	
+	(Get-Content $Path) -Replace $RegEx, $Replacement | Out-File $Path -Encoding UTF8
+}
+
+AssignVersionToFile -Path "$PSScriptRoot\src\LZStringCSharp.csproj" -RegEx "Version>\d+\.\d+\.\d+" -Replacement "Version>$($Version)</"
+AssignVersionToFile -Path "$PSScriptRoot\package.json" -RegEx "`"version`": `"[^`"]+`""-Replacement "`"version`": `"$($Version)`""
+
+Write-Host "Version updated!" -ForegroundColor Green
